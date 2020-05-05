82716af4792296409388f5314175898e8cf696cf
ZhehaoTao <zhehaotao@gmail.com>
2020-05-05 23:32:47 +0800

    updated basic layout for export page

--- src/pages/exportpage/exportpage.component.jsx
+++ src/pages/exportpage/exportpage.component.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import Footer from '../../components/footer/footer.component';
+import ShowType from '../../components/showtype/showtype.component';
+
+import './exportpage.styles.scss';
 
 const ExportPage = () => (
   <div>
-    ExportPage
+    <div className='export-container'>
+      <div className='header-container'>
+        选择图表
+      </div>
+      <div className='subheader-container'>
+        选择要导出的数据
+      </div>
+      <div className='content-container'>
+        <div className='plot-container'>
+          <div className='button-container'>
+            导出
+          </div>
+          <div className='show-container'>
+            <ShowType height='320px' fontSize='8' rotate='0'/>
+          </div>
+          
+        </div>
+        <div className='data-container'>
+          Data
+        </div>
+      </div>
+    </div>
     <Footer />
   </div>
 )
