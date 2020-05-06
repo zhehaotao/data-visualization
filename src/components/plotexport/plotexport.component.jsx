de87bb63e3fa9a83fd39fc3be278d5e735867748
ZhehaoTao <zhehaotao@gmail.com>
2020-05-06 13:24:44 +0800

    added jpg and png export format of plot

--- src/components/plotexport/plotexport.component.jsx
+++ src/components/plotexport/plotexport.component.jsx
@@ -4,22 +4,38 @@ import { Button } from 'antd';
 
 import './plotexport.styles.scss';
 
-class PlotExport extends React.Component {  
-  download = () => {
+class PlotExport extends React.Component {
+  downloadPNG = () => {
     try {
       const canvas = document.getElementsByTagName("canvas");
       canvas[0].toBlob(function(blob){
-      saveAs(blob, "plot.png");
-      },"image/png", 1);
+      saveAs(blob, 'plot.png');
+      });
     } catch (err) {
       alert("Can't find plot");
     }
   }
+
+  downloadJPG = () => {
+    try {
+      const canvas = document.getElementsByTagName("canvas");
+      canvas[0].toBlob(function(blob){
+      saveAs(blob, 'plot.jpg');
+      });
+    } catch (err) {
+      alert("Can't find plot");
+    }
+  }
+
   render () {
     return (
-      <div className='export-container'>
+      <div className='png-container'>
+        <div className='plot'>
+          图表
+        </div>
         <div>
-          <Button onClick={this.download}>PNG导出</Button>
+          <Button type="primary" size='small' onClick={this.downloadPNG}>.png导出</Button>&emsp;
+          <Button type="primary" size='small' onClick={this.downloadJPG}>.jpg导出</Button>
         </div>
       </div>
     )
