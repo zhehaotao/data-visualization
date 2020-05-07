6d87184d5e29293e014afe6f7212a30b6d7d8d13
ZhehaoTao <zhehaotao@gmail.com>
2020-05-07 14:19:26 +0800

    improved UI and event trigger logic for edit data module in make page

--- src/components/edittable/edittable.component.jsx
+++ src/components/edittable/edittable.component.jsx
@@ -1,35 +1,56 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { editData } from '../../redux/editeddata/editeddata.actions';
+import { Button } from 'antd';
+
+import './edittable.styles.scss';
 
 class EditTable extends React.Component {
-  componentDidMount() {
-    const { editData } = this.props
-    document.getElementById('table').onblur = (e) => {
-      const nativeData = e.target.innerText.replace(/[\t\r\n]/g,",").split(",");
-      const newData = [];
-      for (let i = 0; i < this.props.data.length; i++) {
-        let temp = [];
-        for (let j = 0; j < this.props.fields.length; j++) {
-          temp.push(nativeData.shift());
-        }
-        newData.push(temp);
-      }
-      editData(newData)
-    }
-  }
+  // componentDidMount() {
+  //   const { editData } = this.props
+  //   document.getElementById('table').onblur = (e) => {
+  //     const nativeData = e.target.innerText.replace(/[\t\r\n]/g,",").split(",");
+  //     const newData = [];
+  //     for (let i = 0; i < this.props.data.length; i++) {
+  //       let temp = [];
+  //       for (let j = 0; j < this.props.fields.length; j++) {
+  //         temp.push(nativeData.shift());
+  //       }
+  //       newData.push(temp);
+  //     }
+  //     editData(newData)
+  //   }
+  // }
   
-
+  handleClick = () => {
+    const { editData } = this.props
+    const e = document.getElementById('table');
+    const nativeData = e.innerText.replace(/[\t\r\n]/g,",").split(",");
+    const newData = [];
+    for (let i = 0; i < this.props.data.length; i++) {
+      let temp = [];
+      for (let j = 0; j < this.props.fields.length; j++) {
+        temp.push(nativeData.shift());
+      }
+      newData.push(temp);
+    }
+    editData(newData)
+  }
   render () {
     return (
-      <div id='table' contentEditable='true'>
-        <table border="1">
-          <tbody>
-          {this.props.data.map(
-            (row,id) => (<tr key={id}>{row.map(
-                (index,id) => (this.props.fields.includes(id)? (<td key={id}>{index}</td>):null))}</tr>))}
-          </tbody>
-        </table>
+      <div className='modify-container'>
+        <div id='table' contentEditable='true'>
+          <table border="1">
+            <tbody>
+            {this.props.data.map(
+              (row,id) => (<tr key={id}>{row.map(
+                  (index,id) => (this.props.fields.includes(id)? (<td key={id}>{index}</td>):null))}</tr>))}
+            </tbody>
+          </table>
+        </div>
+        <div>
+          <Button type='primary' size='large' onClick={this.handleClick}>完成编辑，生成图表</Button>
+        </div>
       </div>
     )
   }
